ff8806d65b56cbd5f7d22d125153355132180ab1
Nick Anderson <nickanderson@MacBookPro.attlocal.net>
2025-09-19 14:07:19 -0500

    chore: convert run-eslint to CommonJS

---
+++ scripts/run-eslint.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const { ESLint } = require("eslint");
+
+(async function main() {
+  try {
+    const eslint = new ESLint({ ignore: false });
+
+    // lintFiles accepts glob patterns; include JS and HTML files explicitly
+    const results = await eslint.lintFiles(["**/*.js", "**/*.html", "*.js", "*.html"]);
+    const formatter = await eslint.loadFormatter("stylish");
+    const resultText = formatter.format(results);
+    console.log(resultText);
+
+    const errorCount = results.reduce((sum, r) => sum + r.errorCount, 0);
+    process.exit(errorCount > 0 ? 1 : 0);
+  } catch (err) {
+    console.error("ESLint runner error:", err);
+    process.exit(2);
+  }
+})();
